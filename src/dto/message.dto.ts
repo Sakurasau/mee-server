d3cea44802fd6c47861d583318a11e0538147400
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-05 00:34:05 +0200

    refactor: update MessageDto

--- src/dto/message.dto.ts
+++ src/dto/message.dto.ts
@@ -1,6 +1,21 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class MessageDto {
+  // @IsString()
+  // @IsNotEmpty()
+  // id: string;
+
+  // @Type(() => Date)
+  // @IsDate()
+  // @IsNotEmpty()
+  // createdAt: Date;
+
+  // @Type(() => Date)
+  // @IsDate()
+  // @IsOptional()
+  // updatedAt: Date;
+
   @IsString()
   @IsNotEmpty()
   content: string;
