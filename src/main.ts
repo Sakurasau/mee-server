9a7b8680006d7be0f505ee7aad3804de2e8a14e5
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-05 01:01:27 +0200

    feat: configure global prefix using ConfigService and initialize ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { env } from 'process';
+import { ConfigService } from '@nestjs/config';
 
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(app.get(ConfigService).get<string>('VITE_SERVER_BASE_PREFIX'));
   await app.listen(env.PORT_BACKEND);
 }
 bootstrap();
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/controllers/app.controller';
 import { AppService } from '@/services/app.service';
 import { DatabaseModule } from './database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
