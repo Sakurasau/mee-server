62382d8fabefa1be21d3c92c2df1fba299eb76f5
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-05 01:07:51 +0200

    fix: update environment variable names for consistency and adjust NGINX configuration

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(app.get(ConfigService).get<string>('VITE_SERVER_BASE_PREFIX'));
+  app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
   await app.listen(env.PORT_BACKEND);
 }
 bootstrap();
