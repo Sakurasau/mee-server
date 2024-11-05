74673ca5e556b042ce7a34d140269e7b77d6bc83
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-05 15:24:17 +0200

    chore: remove dotenv dependency (unnecessary)

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './modules/app.module';
 import { env } from 'process';
 import { ConfigService } from '@nestjs/config';
 
-require('dotenv').config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
