719011ccd332ddfbe434f79af7436a6051579f8a
mee <nikita.savenko.dev@gmail.com>
2024-11-05 14:14:16 +0000

    fix: rm global prefix from bootstrap backend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
+  // app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
   await app.listen(env.PORT_BACKEND);
 }
 bootstrap();
