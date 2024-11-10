2edf216d8656cb991375b29b98a7385cfe8c67b7
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-10 11:49:00 +0200

    feat: migrate to Fastify platform in NestJS application

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { env } from 'process';
 import { ConfigService } from '@nestjs/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   // app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
   await app.listen(env.PORT_BACKEND);
 }
