3314b624e1790017e48be840ebc20feff8f6d60f
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-16 11:19:09 +0200

    build: add .npmrc (disabling symbolic links) & fix prettier config & fix tsconfig

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  endOfLine: 'auto',
+  useTabs: false,
+}
