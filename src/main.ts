05e5fc6bf6dfc76d5cda4084cbba50634ce08613
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-11-21 00:53:29 +0200

    feat: add and config swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './modules/app/app.module'
 import { ConfigService } from '@nestjs/config'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import * as cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -13,6 +14,16 @@ async function bootstrap() {
     origin: app.get(ConfigService).get('CORS_ORIGIN').split(' '),
     credentials: true,
   })
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mee')
+    .setDescription('Mee Messenger API')
+    .setVersion('1.0')
+    .addTag('messenger')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // app.setGlobalPrefix(app.get(ConfigService).get<string>('SERVER_BASE_PREFIX'));
   await app.listen(
